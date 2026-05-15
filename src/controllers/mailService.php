6717d52b03899fea9b09058a36fa1d6791938c65
Yerosan Bekele <lebrondee20@gmail.com>
2026-05-15 21:51:00 +0300

    changed mailport and mailencryption to use ssl instead of tls, if you are using tls, make sure to change them

--- src/controllers/mailService.php
+++ src/controllers/mailService.php
@@ -1,5 +1,6 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . '/../../vendor/autoload.php';
@@ -7,36 +8,48 @@ require __DIR__ . '/../../vendor/autoload.php';
 function sendVerificationEmail($email, $token) {
     $mail = new PHPMailer(true);
 
-    $mailHost = getenv('MAIL_HOST') ?: 'smtp.gmail.com';
-    $mailPort = getenv('MAIL_PORT') ?: 587;
-    $mailUsername = getenv('MAIL_USERNAME') ?: 'your_email@gmail.com';
-    $mailPassword = getenv('MAIL_PASSWORD') ?: 'your_app_password';
-    $mailEncryption = getenv('MAIL_ENCRYPTION') ?: 'tls';
-    $mailFrom = getenv('MAIL_FROM') ?: $mailUsername;
-    $mailFromName = getenv('MAIL_FROM_NAME') ?: 'PennyWise';
+    $mailHost       = getenv('MAIL_HOST')       ?: 'smtp.gmail.com';
+    $mailPort       = getenv('MAIL_PORT')       ?: 465;
+    $mailUsername   = getenv('MAIL_USERNAME')   ?: 'your_email@gmail.com';
+    $mailPassword   = getenv('MAIL_PASSWORD')   ?: 'your_app_password';
+    $mailEncryption = getenv('MAIL_ENCRYPTION') ?: 'ssl';
+    $mailFrom       = getenv('MAIL_FROM')       ?: $mailUsername;
+    $mailFromName   = getenv('MAIL_FROM_NAME')  ?: 'PennyWise';
 
     try {
         $mail->isSMTP();
-        $mail->Host = $mailHost;
-        $mail->SMTPAuth = true;
-        $mail->Username = $mailUsername;
-        $mail->Password = $mailPassword;
-        $mail->SMTPSecure = $mailEncryption;
-        $mail->Port = $mailPort;
+        $mail->Host       = $mailHost;
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $mailUsername;
+        $mail->Password   = $mailPassword;
+        $mail->Port       = (int) $mailPort;
+
+        // Use SMTPS (implicit SSL on 465) or STARTTLS (explicit TLS on 587)
+        if ($mailEncryption === 'ssl') {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        } else {
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        }
 
         $mail->setFrom($mailFrom, $mailFromName);
         $mail->addAddress($email);
 
-        $link = "http://localhost:8000/verify?token=$token";
+        $appUrl = getenv('APP_URL') ?: 'http://localhost';
+        $link   = "$appUrl/verify?token=$token";
 
         $mail->isHTML(true);
-        $mail->Subject = 'Verify your email';
-        $mail->Body = "Click this link to verify: <a href='$link'>$link</a>";
+        $mail->Subject = 'Verify your PennyWise account';
+        $mail->Body    = "
+            <p>Thanks for signing up for PennyWise!</p>
+            <p>Click the link below to verify your email address:</p>
+            <p><a href='$link'>$link</a></p>
+            <p>If you didn't create an account, you can ignore this email.</p>
+        ";
+        $mail->AltBody = "Verify your email: $link";
 
         $mail->send();
+
     } catch (Exception $e) {
-        $debug = isset($smtpDebug) ? "\nSMTP Debug:\n" . $smtpDebug : '';
-        throw new Exception("Email could not be sent: " . $mail->ErrorInfo . $debug);
+        throw new Exception("Email could not be sent: " . $mail->ErrorInfo);
     }
 }
-?>
